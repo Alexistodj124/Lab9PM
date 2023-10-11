4ef3f3888bc2ade75476d211acb3ccec7b44ed03
Alexistodj124 <alexistodj@gmail.com>
2023-10-10 23:43:13 -0600

    Complete
    No funciona el mutablestateof

--- EventZezziApp/app/build.gradle.kts
+++ EventZezziApp/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -50,8 +52,14 @@ android {
 }
 
 dependencies {
+    val lifecycle_version = "2.6.2"
     val navVersion = "2.7.3"
     val accompanistVersion = "0.32.0"
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // ViewModel utilities for Compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
+
     implementation("androidx.compose.ui:ui-android:1.5.2")
     implementation("com.google.accompanist:accompanist-pager:$accompanistVersion")
     implementation("com.google.accompanist:accompanist-pager-indicators:$accompanistVersion")
--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesScreen.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesScreen.kt
@@ -1,18 +1,36 @@
 package com.zezzi.eventzezziapp.ui.meals.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberImagePainter
+import com.zezzi.eventzezziapp.R
 import com.zezzi.eventzezziapp.navigation.AppBar
 import com.zezzi.eventzezziapp.data.networking.response.MealResponse
+import com.zezzi.eventzezziapp.navigation.NavigationState
+import com.zezzi.eventzezziapp.ui.theme.EventZezziAppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,23 +38,58 @@ fun MealsCategoriesScreen(
     navController: NavController,
     viewModel: MealsCategoriesViewModel = viewModel()
 ) {
-    val rememberedMeals: MutableState<List<MealResponse>> =
-        remember { mutableStateOf(emptyList<MealResponse>()) }
-
-    viewModel.getMeals { response ->
-        val mealsFromTheApi = response?.categories
-        rememberedMeals.value = mealsFromTheApi.orEmpty()
-    }
-
     Scaffold(
         topBar = {
             AppBar(title = "Categories", navController = navController)
         }
     ) {
         LazyColumn(contentPadding = it) {
-            items(rememberedMeals.value) { meal ->
-                Text(text = meal.name)
+            items(viewModel.categories.categories) { meal ->
+                Card(
+                    shape = RoundedCornerShape(8.dp),
+                    elevation = 2.dp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp)
+                ) {
+                    Row {
+                        Image(
+                            painter = rememberImagePainter(meal.imageUrl),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(88.dp)
+                                .padding(4.dp)
+                        )
+                        Column(
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(16.dp)
+                        ) {
+                            Text(
+                                text = meal.id,
+                                style = MaterialTheme.typography.h6
+                            )
+                            Text(
+                                text = meal.name,
+                                style = MaterialTheme.typography.h6
+                            )
+                            Divider()
+                            Text(
+                                text = meal.description,
+                                style = MaterialTheme.typography.h6
+                            )
+                        }
+                    }
+                }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    EventZezziAppTheme {
+        MealsCategoriesScreen(navController = rememberNavController())
     }
 }
